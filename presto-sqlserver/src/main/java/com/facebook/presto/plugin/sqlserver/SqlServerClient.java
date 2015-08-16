a9b25d3a419008b4144e0f6467019dc968a2f84a
ricdong <dongtalk@gmail.com>
2015-08-16 17:31:38 +0800

    add plugin for sqlserver

---
+++ presto-sqlserver/src/main/java/com/facebook/presto/plugin/sqlserver/SqlServerClient.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.plugin.sqlserver;
+
+import com.facebook.presto.plugin.jdbc.BaseJdbcClient;
+import com.facebook.presto.plugin.jdbc.BaseJdbcConfig;
+import com.facebook.presto.plugin.jdbc.JdbcConnectorId;
+
+import javax.inject.Inject;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSet;
+import net.sourceforge.jtds.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+
+import static java.util.Locale.ENGLISH;
+
+/**
+ * Created by ricdong on 15-8-12.
+ */
+public class SqlServerClient extends BaseJdbcClient {
+
+    @Inject
+    public SqlServerClient(JdbcConnectorId connectorId,
+                           BaseJdbcConfig config, SqlServerConfig sqlServerConfig) {
+        super(connectorId, config, "\"", new Driver());
+        int i = 0;
+        sqlServerConfig.getConnectionTimeout();
+        System.err.println("successful to init Sqlserverclient ricdong. fff");
+    }
+
+    @Override
+    public Set<String> getSchemaNames()
+    {
+        // for MySQL, we need to list catalogs instead of schemas
+        try (Connection connection = driver.connect(connectionUrl, connectionProperties);
+             ResultSet resultSet = connection.getMetaData().getCatalogs()) {
+            ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
+            while (resultSet.next()) {
+                String schemaName = resultSet.getString("TABLE_CAT").toLowerCase(ENGLISH);
+                // skip internal schemas
+                if (!schemaName.equals("information_schema") && !schemaName.equals("mysql")) {
+                    schemaNames.add(schemaName);
+                }
+            }
+            return schemaNames.build();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw Throwables.propagate(e);
+        }
+    }
+}
